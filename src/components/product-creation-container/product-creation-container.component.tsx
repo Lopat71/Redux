e7979c12e235c8533530fcb2060668dd8bf4028e
Artem Terekhov <opa4941@gmail.com>
2025-04-22 16:56:39 +0400

    final

--- src/components/product-creation-container/product-creation-container.component.tsx
+++ src/components/product-creation-container/product-creation-container.component.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Modal from "../modal/modal.component";
 import ProductCreationForm from "../product-creation-form/product-creation-form.component";
 import { ProductModel } from "../../data/product.model";
+import { productListData } from "../../data/product.data";
 
 interface ProductCreationContainerProps {
   visible: boolean;
@@ -15,6 +16,19 @@ const ProductCreationContainer: React.FC<ProductCreationContainerProps> = ({
   const handleSubmit = async (product: Partial<ProductModel>) => {
     try {
       console.log("Product submitted:", product);
+      productListData.push({
+        id: 1,
+        title: product.title as string,
+        price: 109.95,
+        description: product.description as string,
+        category: "men's clothing",
+        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        rating: {
+          rate: 3.9,
+          count: 120,
+        },
+      });
+
       onClose();
     } catch (error) {
       console.error("Failed to create product", error);
